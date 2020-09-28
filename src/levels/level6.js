26f02a724c935efe43fb3ac4ebeee59d30519921
Jack Fragassi <jfragassi98@gmail.com>
2020-09-27 20:42:41 -0700

    Make level 6

--- src/levels/level6.js
+++ src/levels/level6.js
@@ -14,18 +14,46 @@ export default [
     startTime: 1000
   },
   {
+    top: -150,
+    left: 300,
+    xVel: 0,
+    yVel: 0.32,
+    startTime: 2500
+  },
+  {
+    top: -150,
+    left: 500,
+    xVel: 0,
+    yVel: 0.32,
+    startTime: 2500
+  },
+  {
     top: 65,
     left: 900,
     xVel: -0.35,
     yVel: 0,
-    startTime: 4000
+    startTime: 3500
   },
   {
     top: 285,
     left: 900,
     xVel: -0.35,
     yVel: 0,
-    startTime: 4000
+    startTime: 3500
+  },
+  {
+    top: 500,
+    left: 300,
+    xVel: 0,
+    yVel: -0.32,
+    startTime: 5000
+  },
+  {
+    top: 500,
+    left: 500,
+    xVel: 0,
+    yVel: -0.32,
+    startTime: 5000
   },
   {
     top: -150,
@@ -60,12 +88,87 @@ export default [
     startTime: 10000
   },
   {
-    top: 200,
-    left: 400,
+    top: -150,
+    left: 200,
     xVel: 0,
+    yVel: 0.36,
+    startTime: 12000
+  },
+  {
+    top: 150,
+    left: -100,
+    xVel: 0.42,
     yVel: 0,
-    angularVel: 0.3,
-    startTime: 1000,
-    scaleTime: 3000
+    startTime: 12700
+  },
+  {
+    top: 500,
+    left: 600,
+    xVel: 0,
+    yVel: -0.36,
+    startTime: 13500
+  },
+  {
+    top: 270,
+    left: 900,
+    xVel: -0.42,
+    yVel: 0,
+    startTime: 14200
+  },
+  {
+    top: -150,
+    left: 200,
+    xVel: 0,
+    yVel: 0.36,
+    startTime: 15000
+  },
+  {
+    top: 150,
+    left: -100,
+    xVel: 0.42,
+    yVel: 0,
+    startTime: 15800
+  },
+  {
+    top: 500,
+    left: 600,
+    xVel: 0,
+    yVel: -0.36,
+    startTime: 16600
+  },
+  {
+    top: 270,
+    left: 900,
+    xVel: -0.42,
+    yVel: 0,
+    startTime: 17400
+  },
+  {
+    top: -150,
+    left: 200,
+    xVel: 0,
+    yVel: 0.36,
+    startTime: 18200
+  },
+  {
+    top: 150,
+    left: -100,
+    xVel: 0.42,
+    yVel: 0,
+    startTime: 19000
+  },
+  {
+    top: 500,
+    left: 600,
+    xVel: 0,
+    yVel: -0.36,
+    startTime: 19800
+  },
+  {
+    top: 270,
+    left: 900,
+    xVel: -0.42,
+    yVel: 0,
+    startTime: 20600
   },
 ];
